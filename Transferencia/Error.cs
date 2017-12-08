4651607f33928de6fca35263f3159b04391b3b0a
Samuel Barrera <sbarrera@gmail.com>
2017-12-08 18:03:54 -0300

    Actualizacion

---
+++ Transferencia/Error.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Transferencia
+{
+    using System;
+    using System.Xml.Serialization;
+
+    [Serializable]
+    [XmlRoot("Error")]
+    public class Error
+    {
+        [XmlElement("Id", Type = typeof(long))]
+        public long Id { set; get; }
+
+        [XmlElement("IdNivel", Type = typeof(long))]
+        public long IdNivel { set; get; }
+
+        [XmlElement("Codigo", Type = typeof(string))]
+        public string Codigo { set; get; }
+
+        [XmlElement("Nombre", Type = typeof(string))]
+        public string Nombre { set; get; }
+
+        [XmlElement("Descripcion", Type = typeof(string))]
+        public string Descripcion { set; get; }
+
+        [XmlElement("FechaCreacion", Type = typeof(DateTime))]
+        public DateTime FechaCreacion { set; get; }
+
+        [XmlElement("FechaActualizacion", Type = typeof(DateTime))]
+        public DateTime FechaActualizacion { set; get; }
+    }
+}
