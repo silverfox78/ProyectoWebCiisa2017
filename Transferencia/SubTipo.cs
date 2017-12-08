eb651d5f603fee6021a1282cf41e2b1a008f4883
victarion1 <crls.hdlg@gmail.com>
2017-12-08 18:48:26 -0300

    setters y getters creados

--- Transferencia/SubTipo.cs
+++ Transferencia/SubTipo.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace Transferencia
 {
-    class SubTipo
+
+    using System;
+    using System.Xml.Serialization;
+
+    [Serializable]
+    [XmlRoot("SubTipo")]
+  public class SubTipo
     {
+        [XmlElement("Id", Type = typeof(long))]
+        public long Id { set; get; }
+
+        [XmlElement("Codigo", Type = typeof(string))]
+        public string CodigoTipo { set; get; }
+
+        [XmlElement("Nombre", Type = typeof(string))]
+        public string NombreTipo { set; get; }
+
+        [XmlElement("Descripcion", Type = typeof(string))]
+        public string DescripcionTipo { set; get; }
+
+        [XmlElement("FechaCreacion", Type = typeof(string))]
+        public string FechCreacion { set; get; }
+
+        [XmlElement("FechaCreacion", Type = typeof(string))]
+        public string FechCareacion { set; get; }
+
+        [XmlElement("FechaActualizacion", Type = typeof(string))]
+        public string FechaActualizacion { set; get; }
+
+        [XmlElement("Estado", Type = typeof(string))]
+        public string Estado { set; get; }
     }
 }
