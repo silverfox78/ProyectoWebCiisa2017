7200bdf1fb2687aca078ebdac49a9777092e1b4f
Samuel Barrera <sbarrera@gmail.com>
2017-12-09 01:53:53 -0300

    Actualizacion

--- Sitio/Tipo.aspx.cs
+++ Sitio/Tipo.aspx.cs
@@ -1,16 +1,53 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class Default2 : System.Web.UI.Page
+public partial class Tipo : System.Web.UI.Page
 {
-    protected void Page_Load(object sender, EventArgs e)
+    [System.Web.Services.WebMethod]
+    public static string ListaGrilla()
     {
-        
+        string rutaArchivo = HttpContext.Current.Server.MapPath("/Archivos/tipos.xml");
+        return JsonConvert.SerializeObject(new Negocio.Tipo(rutaArchivo).Listar());
+    }
 
+    [System.Web.Services.WebMethod]
+    public static string Buscar(long id)
+    {
+        string rutaArchivo = HttpContext.Current.Server.MapPath("/Archivos/tipos.xml");
+        return JsonConvert.SerializeObject(new Negocio.Tipo(rutaArchivo).Buscar(id));
+    }
 
+    [System.Web.Services.WebMethod]
+    public static string Guardar(long id, string codigo, string nombre, string descripcion, string estado)
+    {
+        string rutaArchivo = HttpContext.Current.Server.MapPath("/Archivos/tipos.xml");
+
+        Transferencia.Tipo tipo =
+                new Transferencia.Tipo()
+                {
+                    Id = id, 
+                    Codigo = codigo,
+                    Nombre = nombre,
+                    Descripcion = descripcion,
+                    FechaCreacion = DateTime.Now,
+                    FechaActualizacion = DateTime.Now,
+                    Estado = estado
+                };
+
+        new Negocio.Tipo(rutaArchivo).Guarda(tipo);
+        return JsonConvert.SerializeObject(true);
+    }
+
+    [System.Web.Services.WebMethod]
+    public static string Eliminar(long id)
+    {
+        string rutaArchivo = HttpContext.Current.Server.MapPath("/Archivos/tipos.xml");
+        new Negocio.Tipo(rutaArchivo).Eliminar(id);
+        return JsonConvert.SerializeObject(true);
     }
 }
