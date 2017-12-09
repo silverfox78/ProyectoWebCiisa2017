78ac8a17531b700961ef16d5c88a02d825e04ec5
Samuel Barrera <sbarrera@gmail.com>
2017-12-09 01:27:24 -0300

    Actualizacion

---
+++ Negocio/Nivel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class Nivel
+    {
+        private Base.Nivel Accesobase;
+
+        public Nivel(string rutaArchivo)
+        {
+            this.Accesobase = new Base.Nivel(rutaArchivo);
+        }
+
+        public Transferencia.Nivel Buscar(long id)
+        {
+            return this.Accesobase.Buscar(id);
+        }
+
+        public void Eliminar(long id)
+        {
+            this.Accesobase.Eliminar(id);
+        }
+
+        public void Guarda(Transferencia.Nivel usuario)
+        {
+            this.Accesobase.Guardar(usuario);
+        }
+
+        public List<Transferencia.Nivel> Listar()
+        {
+            return this.Accesobase.Listar();
+        }
+    }
+}
